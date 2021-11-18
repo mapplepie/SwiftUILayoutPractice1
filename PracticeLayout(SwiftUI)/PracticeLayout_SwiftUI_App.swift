cf9749c3ce05ebcd62307377662f3364eacbcb75
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-18 16:03:23 +0900

    Initial Commit

---
+++ PracticeLayout(SwiftUI)/PracticeLayout_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeLayout_SwiftUI_App.swift
+//  PracticeLayout(SwiftUI)
+//
+//  Created by Mai Uchida on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct PracticeLayout_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
